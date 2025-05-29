eb5098a77b60b0e2813c5ee4672601e49fd4cb8b
Nahary Toavina <toavina.rabenjanaharisoa@gmail.com>
2025-05-29 11:48:20 +0300

    add laneLines for a route

--- main.cpp
+++ main.cpp
@@ -1,34 +1,46 @@
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/Graphics/RectangleShape.hpp>
-#include <SFML/Graphics/Color.hpp>
-#include <SFML/Window/Event.hpp>
-#include <SFML/Window/VideoMode.hpp>
-
+#include <SFML/Graphics.hpp>
+using namespace sf;
 int main() {
     const int windowWidth = 800;
     const int windowHeight = 600;
     const int roadWidth = 500;
+    const int laneCount = 4;
+    const float laneWidth = static_cast<float>(roadWidth) / laneCount;
 
-    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Car Game Retro");
+    // RenderWindow window(VideoMode(static_cast<unsigned int>(windowWidth), static_cast<unsigned int>(windowHeight)), "Car Game Retro");
+    RenderWindow window(VideoMode({windowWidth, windowHeight}), "Cars");
     window.setFramerateLimit(60);
 
-    sf::RectangleShape road(sf::Vector2f(static_cast<float>(roadWidth), static_cast<float>(windowHeight)));
-    road.setFillColor(sf::Color(50, 50, 50));
-    road.setPosition(0.f, 0.f);
+    RectangleShape road(Vector2f(static_cast<float>(roadWidth), static_cast<float>(windowHeight)));
+    road.setFillColor(Color(50, 50, 50));
+    road.setPosition(Vector2f(0.f, 0.f));
 
-    sf::RectangleShape menu(sf::Vector2f(static_cast<float>(windowWidth - roadWidth), static_cast<float>(windowHeight)));
-    menu.setFillColor(sf::Color(30, 30, 30));
-    menu.setPosition(static_cast<float>(roadWidth), 0.f);
+    RectangleShape menu(Vector2f(static_cast<float>(windowWidth - roadWidth), static_cast<float>(windowHeight)));
+    menu.setFillColor(Color(30, 30, 30));
+    menu.setPosition(Vector2f(static_cast<float>(roadWidth), 0.f));
+
+    std::vector<RectangleShape> laneLines;
+    for (int i = 1; i < laneCount; ++i) {
+        RectangleShape line(Vector2f(4.f, static_cast<float>(windowHeight)));
+        line.setFillColor(Color::White);
+        line.setPosition(Vector2f(i * laneWidth - 2.f, 0.f));
+        laneLines.push_back(line);
+    }
 
     while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+
+        while (std::optional event = window.pollEvent()) {
+            if (event->is<Event::Closed>()) {
                 window.close();
+            }
         }
 
         window.clear();
         window.draw(road);
+
+        for (auto& line : laneLines)
+            window.draw(line);
+
         window.draw(menu);
         window.display();
     }
